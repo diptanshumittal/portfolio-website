1f3a9da0a83a1359b4b005fc77640974b3b00001
diptanshumittal <diptanshu18232@iiitd.ac.in>
2021-04-02 07:26:21 +0530

    Updates

---
+++ precache-manifest.78a8ccf482868181cddccadbb4a3e4a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1038bc9f14558980f3cc7b9c52269d87",
+    "url": "/portfolio-website/index.html"
+  },
+  {
+    "revision": "39eba7edb759c9e058de",
+    "url": "/portfolio-website/static/css/main.0d65621a.chunk.css"
+  },
+  {
+    "revision": "07bdc05c075a3f17ca4b",
+    "url": "/portfolio-website/static/js/2.c9262f6f.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/portfolio-website/static/js/2.c9262f6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39eba7edb759c9e058de",
+    "url": "/portfolio-website/static/js/main.4ab5455a.chunk.js"
+  },
+  {
+    "revision": "99cb685b86efd0f580da",
+    "url": "/portfolio-website/static/js/runtime-main.c16362e1.js"
+  },
+  {
+    "revision": "b7de42a5dd5a13280042a55f4b4459de",
+    "url": "/portfolio-website/static/media/profilpic.b7de42a5.png"
+  }
+]);
